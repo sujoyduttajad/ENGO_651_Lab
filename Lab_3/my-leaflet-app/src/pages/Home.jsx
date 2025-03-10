395809eb6ac6295e3eaa7616e34767a5f490c487
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2025-03-10 02:12:45 -0600

    updated leaflet popup with details

--- Lab_3/my-leaflet-app/src/pages/Home.jsx
+++ Lab_3/my-leaflet-app/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { formatDate } from "../utils/functions";
 
 const Home = () => {
   const [geoData, setGeoData] = useState(null);
@@ -20,8 +21,36 @@ const Home = () => {
   }, []);
 
   const onEachFeature = (feature, layer) => {
-    if (feature.properties && feature.properties.permitnum) {
-      layer.bindPopup(`Permit Number: ${feature.properties.permitnum}`);
+    if (feature.properties) {
+      const popupContent = `
+         <div style="padding: 16px; font-family: 'Roboto', sans-serif;">
+          <h2 style="margin: 0; font-size: 1.25rem;">Permit Details</h2>
+          <p style="margin: 8px 0;"><strong>Permit Number:</strong> ${
+            feature.properties.permitnum
+          }</p>
+          <p style="margin: 8px 0;"><strong>Community Name:</strong> ${
+            feature.properties.communityname
+          }</p>
+
+          <p style="margin: 8px 0;"><strong>Issue Date:</strong> ${
+            feature.properties.issueddate === null
+              ? "Not Available"
+              : formatDate(feature.properties.issueddate)
+          }</p>
+          <p style="margin: 8px 0;"><strong>Contractor Name:</strong> ${
+            feature.properties.contractorname === null
+              ? "Not Available"
+              : feature.properties.contractorname
+          }</p>
+          <p style="margin: 8px 0;"><strong>Workclass Group:</strong> ${
+            feature.properties.workclassgroup
+          }</p>
+          <p style="margin: 8px 0;"><strong>Original Address:</strong> ${
+            feature.properties.originaladdress
+          }</p>
+        </div>
+      `;
+      layer.bindPopup(popupContent);
     }
   };
 

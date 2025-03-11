364e84c83423794199e1489143a9708ca0ed614d
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2025-03-11 01:21:29 -0600

    Added DateRangePicker component

--- Lab_3/my-leaflet-app/src/components/Header.jsx
+++ Lab_3/my-leaflet-app/src/components/Header.jsx
@@ -1,26 +1,35 @@
-import { AppBar, Button } from "@mui/material";
+import { AppBar, Box, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
+import DateRangePicker from "./DateRangePicker";
 
-const Header = () => {
+const Header = ({ fetchPermits }) => {
   return (
     <nav className="header">
-      <Paper
-        component="form"
-        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
-      >
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="Search Building Permits"
-          inputProps={{ "aria-label": "search google maps" }}
-        />
-        <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
-          <SearchIcon />
-        </IconButton>
-      </Paper>
+      <Box display="flex" justifyContent="space-evenly" alignItems="center">
+        {/* <Paper
+          component="form"
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            width: 400,
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Search Building Permits"
+            inputProps={{ "aria-label": "search google maps" }}
+          />
+          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Paper> */}
+        <DateRangePicker onDateChange={fetchPermits} />
+      </Box>
 
       <div>
         <Link to="/" style={{ color: "white", marginRight: "15px" }}>
